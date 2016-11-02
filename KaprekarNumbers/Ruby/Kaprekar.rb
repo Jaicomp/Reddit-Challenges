7e319a4252440355868ed89c9a4c15dd375bbfba
Jaicomp <jaicompdeveloper@gmail.com>
2016-11-02 08:18:53 +0100

    Completed KaprekarNumbers in Ruby

--- KaprekarNumbers/Ruby/Kaprekar.rb
+++ KaprekarNumbers/Ruby/Kaprekar.rb
@@ -1,47 +1,45 @@
-puts "Introduce range to calculate Kaprekar Numbers: "
-rangeKaprekarStringFormat = gets
+		puts "Introduce range to calculate Kaprekar Numbers: "
+		rangeKaprekarStringFormat = gets
 
-rangeKaprekar = rangeKaprekarStringFormat.scan(/[0-9]*/)
-rangeKaprekar.delete_if{|i| i==""}
+		rangeKaprekar = rangeKaprekarStringFormat.scan(/[0-9]*/)
+		rangeKaprekar.delete_if{|i| i==""}
 
-startRange = Integer(rangeKaprekar[0])
-endRange = Integer(rangeKaprekar[1])
+		startRange = Integer(rangeKaprekar[0])
+		endRange = Integer(rangeKaprekar[1])
 
-for i in startRange..endRange
+		for i in startRange..endRange
 
-	squareIndex = String(i**2)
+			squareIndex = String(i**2)
 
-	begin
+			begin
 		
-		prefixNum = squareIndex[0 .. (squareIndex.length/2 - 1)]
-		sufixNum = squareIndex[(squareIndex.length/2) .. (squareIndex.length)]
+				for j in 0..squareIndex.length
+					prefixNum = squareIndex[0 .. j]
+					sufixNum = squareIndex[(j+1) .. (squareIndex.length)]
 
-		# Remove octal representation in String, i.e "008" -> "8"
-		index = 0
-		while prefixNum[index] == '0'
-			prefixNum[index] = ''
-			index += 1
-		end
+					# Remove octal representation in String, i.e "008" -> "8"
+					while prefixNum[0] == '0'
+						prefixNum[0] = ''
+					end
 		
-		# Remove octal representation in String, i.e "008" -> "8"
-		index = 0
-		while sufixNum[index] == '0'
-			sufixNum[index] = ''
-			index += 1
+					# Remove octal representation in String, i.e "008" -> "8"
+					while sufixNum[0] == '0'
+						sufixNum[0] = ''
+					end
+			
+					prefixNum = Integer(prefixNum)
+					sufixNum = Integer(sufixNum)
+
+					if (prefixNum + sufixNum)  == i
+						puts i
+					end
+				end
+			rescue Exception=>e
+			end
+
+
+
 		end
-
-		prefixNum = Integer(prefixNum)
-		sufixNum = Integer(sufixNum)
-
-		if (prefixNum + sufixNum)  == i
-			puts i
-		end
-	rescue Exception=>e
-	end
-
-
-
-end
 
 
 
